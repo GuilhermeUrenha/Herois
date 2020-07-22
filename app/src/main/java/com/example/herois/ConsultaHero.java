13aa02cba2e6daecbbbdc014ad14dcdd7a68dd69
GuilhermeUrenha <guilherme.urenha@gmail.com>
2020-07-22 20:10:01 -0300

    Kek

--- app/src/main/java/com/example/herois/ConsultaHero.java
+++ app/src/main/java/com/example/herois/ConsultaHero.java
@@ -3,22 +3,21 @@ package com.example.herois;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
-import org.json.JSONException;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class ConsultaHero extends AppCompatActivity {
 
-    private EditText etNome = this.findViewById(R.id.etNome);
-    private EditText etCod = this.findViewById(R.id.etCod);
-    private EditText etEspec = this.findViewById(R.id.etEspec);
-    private EditText etHab = this.findViewById(R.id.etHab);
-    private EditText etVulneb = this.findViewById(R.id.etVulneb);
-    private EditText etNivel = this.findViewById(R.id.etNivel);
-    private EditText etEquip = this.findViewById(R.id.etEquip);
-    private EditText etDesc = this.findViewById(R.id.etDesc);
+    Intent intent = this.getIntent();
+    Bundle bundle = intent.getBundleExtra("Extra");
+
+    TableLayout tl = (TableLayout)findViewById(R.id.table);
+    TableRow tablerow1 = new TableRow(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
